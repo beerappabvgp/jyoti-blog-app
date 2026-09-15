54888c56a741a4f5be6577568a44cf8b86ef7da2
beerappabvgp <beerappabharathb@gmail.com>
2026-09-15 11:31:49 +0530

    updated cors

--- app.js
+++ app.js
@@ -8,7 +8,10 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: true,
+    credentials: true,
+}));
 app.use(express.json());
 app.use('/api/v1/users', router);
 app.use('/api/v1/blogs', blogRouter);
