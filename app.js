73a411acd12fa6e60cc2066f628a78ae5f5b0638
beerappabvgp <beerappabharathb@gmail.com>
2026-09-15 11:25:42 +0530

    feat: enable CORS to allow all origins

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import dotenv from 'dotenv';
 import { connectToDatabase } from './src/db/connectToDB.js';
 import { router } from './src/routes/userRoutes.js';
@@ -7,6 +8,7 @@ dotenv.config();
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/api/v1/users', router);
 app.use('/api/v1/blogs', blogRouter);
